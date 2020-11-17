0afa34989e413667d1d7e12d9c1e20da9cdabd2f
paldhous <peter@peteraldhous.com>
2020-11-17 15:40:48 -0800

    edited data

---
+++ games.R
@@ -0,0 +1,44 @@
+# load required packages
+library(tidyverse)
+library(gganimate)
+
+# load data
+games <- read_csv("data/vgsales-12-4-2019.csv")
+
+# initial processing
+games <- games %>%
+  mutate(sales = case_when(!is.na(Total_Shipped) ~ Total_Shipped,
+                           TRUE ~ Global_Sales),
+         img_url_full = paste0("http://www.vgchartz.com/",img_url))
+
+write_csv(games, "data/games_edit.csv", na = "")
+
+# how many of each category?
+unique(games$Genre) # 20
+unique(games$Platform) # 74
+unique(games$Publisher) # 3068
+
+# data to make join tables for simplification of categories
+sales_genre <- games %>%
+  group_by(Genre) %>%
+  summarize(sales = sum(sales, na.rm = TRUE)) %>%
+  arrange(desc(sales))
+write_csv(games, "data/sales_genre.csv", na = "")
+
+sales_platform <- games %>%
+  group_by(Platform) %>%
+  summarize(sales = sum(sales, na.rm = TRUE)) %>%
+  arrange(desc(sales))
+write_csv(games, "data/sales_platform.csv", na = "")
+
+sales_publisher <- games %>%
+  group_by(Publisher) %>%
+  summarize(sales = sum(sales, na.rm = TRUE)) %>%
+  arrange(desc(sales))
+write_csv(games, "data/sales_publisher.csv", na = "")
+
+
+
+
+
+
